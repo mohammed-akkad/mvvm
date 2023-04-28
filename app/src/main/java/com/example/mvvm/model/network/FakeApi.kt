8fc0ce8832c633114b4d352d569e85b4365ec636
mohammed-akkad <momoo362514@gmail.com>
2023-04-28 20:04:24 +0300

    rx java with view model

--- app/src/main/java/com/example/mvvm/model/network/FakeApi.kt
+++ app/src/main/java/com/example/mvvm/model/network/FakeApi.kt
@@ -1,5 +1,10 @@
 package com.example.mvvm.model.network
 
+import android.annotation.SuppressLint
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
+
 class FakeApi {
     private val listOfString = listOf(
         "user-ar",
@@ -7,7 +12,9 @@ class FakeApi {
         "user-ru",
         "user-ch"
     )
-     fun getNameUser(): String{
-       return listOfString.random()
+     @SuppressLint("SuspiciousIndentation")
+     fun getNameUser(): Single<String>{
+       val username =  listOfString.random()
+         return Single.just(username).delay(5,TimeUnit.SECONDS)
     }
 }
--- app/src/main/java/com/example/mvvm/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm/viewModel/MainViewModel.kt
@@ -4,14 +4,34 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mvvm.model.network.FakeApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainViewModel: ViewModel() {
     private val fakeApi = FakeApi()
     private val _currentUser = MutableLiveData<String>()
+    private val compositeDisposable = CompositeDisposable()
     val currentUser: LiveData<String>
         get() = _currentUser
 
     fun getUserName(){
-        _currentUser.postValue(fakeApi.getNameUser())
+        fakeApi.getNameUser()
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe(::getUserNameSuccess).add(compositeDisposable)
+    }
+    private fun getUserNameSuccess(name: String){
+        _currentUser.postValue(name)
+
+    }
+    private fun Disposable.add(compositeDisposable: CompositeDisposable){
+        compositeDisposable.add(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
     }
 }
